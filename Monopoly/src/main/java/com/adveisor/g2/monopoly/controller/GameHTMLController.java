dd3896e94cc0b63b6daef3f316cbf163d9d5c894
Mingxuan Qin <qmx0921@gmail.com>
2022-06-07 10:46:43 +0200

    use freemarker to serve html webpage

--- Monopoly/src/main/java/com/adveisor/g2/monopoly/controller/GameHTMLController.java
+++ Monopoly/src/main/java/com/adveisor/g2/monopoly/controller/GameHTMLController.java
@@ -5,10 +5,21 @@
 package com.adveisor.g2.monopoly.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * The controller for serving html webpage to the client
  */
 @Controller
 public class GameHTMLController {
+
+    @GetMapping(value = "/chat", produces = "text/html")
+    public String chatPage() {
+        return "chat";
+    }
+
+    @GetMapping(value = "/login", produces = "text/html")
+    public String loginPage() {
+        return "/login-page/index";
+    }
 }
