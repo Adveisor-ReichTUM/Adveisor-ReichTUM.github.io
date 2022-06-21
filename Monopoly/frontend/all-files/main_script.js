492bbc494480740b23d38f6d925c490b4b8a474d
Fabian_Gruber <gruberfabian@gmx.net>
2022-06-21 23:07:39 +0200

    .

--- Monopoly/frontend/all-files/main_script.js
+++ Monopoly/frontend/all-files/main_script.js
@@ -524,7 +524,7 @@ function disable_trading_page() {
     document.getElementById("Verhandeln").setAttribute('onclick', 'null');
     document.getElementById("Straßen").setAttribute('onclick', 'null');
     //Graue den Bildschirm ein
-    document.body.style.opacity = 0.1;
+    document.body.style.opacity = 0.3;
     document.body.style.background.setAttribute('background-blend-mode', 'overlay');
 }
 
